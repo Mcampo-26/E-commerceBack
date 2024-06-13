d514dbe2e4cd5e0aa33c0771c06d714f4fb06daa
Mauricio Campo <mcampo26@gmail.com>
2024-06-12 21:30:52 -0300

    agrego respuesta del controller carrito

--- src/controllers/carritoControllers/index.js
+++ src/controllers/carritoControllers/index.js
@@ -1,18 +1,47 @@
 import Carrito from '../../models/Carrito.js';
 import Productos from "../../models/Productos.js";
 
+
 export const getCarrito = async (req, res) => {
   try {
+    console.log('getCarrito called');
     const { userId } = req.params;
+    console.log('User ID:', userId);
+
     const carrito = await Carrito.findOne({ user: userId }).populate('items.product');
+    console.log('Carrito encontrado:', carrito);
+
     if (!carrito) {
+      console.log('No se encontró el carrito');
       return res.status(404).json({ error: 'No se encontró el carrito' });
     }
-    res.status(200).json(carrito);
+
+    // Filtrar los productos nulos del carrito
+    const filteredItems = carrito.items.filter(item => item.product !== null);
+    console.log('Filtered Items:', filteredItems);
+
+    // Calcular la cantidad total de items en el carrito
+    const totalItems = filteredItems.reduce((acc, item) => acc + item.quantity, 0);
+    console.log('Total Items:', totalItems);
+
+    // Crear un nuevo objeto de carrito con los items filtrados y la cantidad total de items
+    const filteredCarrito = {
+      ...carrito.toObject(),
+      items: filteredItems,
+      totalItems: totalItems,  // Agregar la cantidad total de items al carrito
+    };
+
+    console.log('Filtered Carrito:', filteredCarrito);
+
+    res.status(200).json(filteredCarrito);
   } catch (error) {
+    console.log('Error:', error.message);
     res.status(500).json({ error: error.message });
   }
 };
+
+
+
 
 export const createCarrito = async (req, res) => {
   try {
